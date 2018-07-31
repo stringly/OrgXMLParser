10dacc5443cae1c27e8f8ceba1e586ebf1d8997f
stringly <jcs3082@hotmail.com>
2018-07-30 23:22:39 -0400

    Initial

--- OrgXMLParser/Program.cs
+++ OrgXMLParser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace OrgXMLParser
     {
         static void Main(string[] args)
         {
+            string path = args[0];
+
+            Console.WriteLine("Attempting to parse " + path);
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File found!");
+
+            }
+            else
+            {
+                Console.WriteLine("File could not be found. Confirm the filepath and try again.");                
+            }
+
         }
     }
 }
