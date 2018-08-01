4727ff5c1d645640f614829727983e71e18c829c
stringly <jcs3082@hotmail.com>
2018-07-31 23:38:07 -0400

    Stuff

---
+++ OrgXMLParser/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrgXMLParser
+{
+    class Component
+    {
+        public int ComponentID { get; set; }
+        public int ParentComponentID { get; set; }
+        public string ComponentName { get; set; }
+        public string ParentComponentName { get; set; }
+        public List<Component> DirectChildren { get; set; }
+
+        public Component()
+        {
+            this.DirectChildren = new List<Component>();
+        }
+
+    }
+}
--- OrgXMLParser/Program.cs
+++ OrgXMLParser/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace OrgXMLParser
 {
@@ -11,19 +14,101 @@ namespace OrgXMLParser
     {
         static void Main(string[] args)
         {
-            string path = args[0];
 
-            Console.WriteLine("Attempting to parse " + path);
-            if (File.Exists(path))
+        Console.WriteLine(@"Attempting to parse Query from F:\Projects\BlueDeck\OrgChart1.accdb" );
+            if (File.Exists(@"F:\Projects\BlueDeck\OrgChart1.accdb"))
             {
                 Console.WriteLine("File found!");
+                // Obv, change this to any function that can generate a "flat" List<Component>
+                ProcessAccessData();
+                bool exit = false;
+                do
+                {
+                    Console.WriteLine("Valid Commands:\n\t[unitname] will attempt to parse a unit tree with the name provided" +
+                        "\n\t[exit] will exit the program");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
 
+                        default:
+                            Console.WriteLine("Command not recognized.\n");
+                            break;
+
+                    }
+                } while (!exit);
+                
             }
             else
             {
-                Console.WriteLine("File could not be found. Confirm the filepath and try again.");                
+                Console.WriteLine("File could not be found. Confirm the data source exists at the filepath and try again.");                
             }
 
         }
+        // This creates a List<Component> flat list of components by connecting to Access
+        static void ProcessAccessData()
+        {
+            OleDbConnection conn = new
+                OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+                @"Data source= F:\Projects\BlueDeck\OrgChart1.accdb";
+            try
+            {
+                List<Component> rawComponentList = new List<Component>();
+                conn.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM District_I_Map", conn);
+                reader = cmd.ExecuteReader();
+                while (reader.Read()) {
+                    Component n = new Component();
+                    n.ComponentID = Convert.ToInt32(reader[0]);
+                    n.ComponentName = reader[2].ToString();
+                    n.ParentComponentID = Convert.ToInt32(reader[1]);
+                    n.ParentComponentName = reader[3].ToString();
+                    Console.WriteLine(string.Format($"\nAdding {n.ComponentName} to Component List..."));
+                    rawComponentList.Add(n);
+                }
+                // All Components should be added to the collection at this point
+                // Call the function to parse and nest components from the flat list
+                NestFlatComponentList(rawComponentList);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect to data source");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        static void NestFlatComponentList(List<Component> ls)
+        {
+            int i = 0;
+            foreach (Component current in ls)
+            {
+                i++;
+                Console.WriteLine($"Processing component number {i}, current master list count is: " + ls.Count);
+
+
+                // Gather My Children
+                //List<Component> childList = new List<Component>();
+                foreach (Component child in ls)
+                {
+                    if (child.ParentComponentID == current.ComponentID)
+                    {
+                        //childList.Add(child);
+                        current.DirectChildren.Add(child);
+                    }
+                }
+            }
+            var j = 0;
+            foreach (Component x in ls)
+            {
+                j++;
+                Console.WriteLine($"List item #{j} is {x.ComponentName} and has {x.DirectChildren?.Count()} children.");
+            }
+        }
     }
+
 }
